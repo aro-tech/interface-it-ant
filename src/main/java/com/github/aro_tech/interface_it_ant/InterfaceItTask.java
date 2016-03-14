8b2a628667ed4a77824d5583f5abd34029f73fad
aro_tech <aro_tech@gmx.com>
2016-03-14 10:29:34 +0100

    Implement argument source creation from parameters

--- src/main/java/com/github/aro_tech/interface_it_ant/InterfaceItTask.java
+++ src/main/java/com/github/aro_tech/interface_it_ant/InterfaceItTask.java
@@ -5,11 +5,15 @@ package com.github.aro_tech.interface_it_ant;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
 
 import org.apache.tools.ant.BuildException;
 import org.apache.tools.ant.Task;
 import org.interfaceit.ClassCodeGenerator;
 import org.interfaceit.meta.arguments.ArgumentNameSource;
+import org.interfaceit.meta.arguments.LookupArgumentNameSource;
+import org.interfaceit.meta.arguments.SourceLineReadingArgumentNameLoader;
+import org.interfaceit.util.FileUtils;
 
 /**
  * Custom Ant task to generate a wrapper interface delegating to static methods
@@ -18,6 +22,8 @@ import org.interfaceit.meta.arguments.ArgumentNameSource;
  *
  */
 public class InterfaceItTask extends Task {
+	private String sourceArchivePath;
+	private String delegateClass;
 
 	/*
 	 * (non-Javadoc)
@@ -27,11 +33,14 @@ public class InterfaceItTask extends Task {
 	@Override
 	public void execute() throws BuildException {
 		validateAttributes();
+
+		// System.out.println("Hello, Ant");
 		ClassCodeGenerator generator = makeInterfaceItGenerator();
+
 		try {
-			generator.generateClassToFile(getOutputDirectory(), getTargetInterfaceName(), getDelegateClass(),
+			generator.generateClassToFile(getOutputDirectory(), getTargetInterfaceName(), getDelegateClassObject(),
 					getTargetPackageName(), makeArgumentNameSource(), getIndentationSpaces());
-		} catch (IOException e) {
+		} catch (IOException | ClassNotFoundException e) {
 			throw new BuildException(e);
 		}
 	}
@@ -40,16 +49,34 @@ public class InterfaceItTask extends Task {
 		return 0;
 	}
 
-	protected ArgumentNameSource makeArgumentNameSource() {
-		return null;
+	protected ArgumentNameSource makeArgumentNameSource() throws IOException, ClassNotFoundException {
+		LookupArgumentNameSource nameSource = new LookupArgumentNameSource();
+		File archiveFile = getArchiveFile();
+		if (null != archiveFile && archiveFile.exists()) {
+			List<String> sourceLines = new FileUtils().readFilesInZipArchive(archiveFile,
+					this.getDelegateClassObject().getName().replace('.', '/') + ".java");
+			new SourceLineReadingArgumentNameLoader().parseAndLoad(sourceLines, nameSource);
+			return nameSource;
+		}
+		return new ArgumentNameSource() {
+		};
+	}
+
+
+	private File getArchiveFile() {
+		if(null == this.getSourceArchivePath()) {
+			return null;
+		}
+		File archiveFile = new File(this.getSourceArchivePath());
+		return archiveFile;
 	}
 
 	private String getTargetPackageName() {
 		return null;
 	}
 
-	private Class<?> getDelegateClass() {
-		return null;
+	private Class<?> getDelegateClassObject() throws ClassNotFoundException {
+		return Class.forName(this.getDelegateClass());
 	}
 
 	private String getTargetInterfaceName() {
@@ -65,6 +92,36 @@ public class InterfaceItTask extends Task {
 
 	protected ClassCodeGenerator makeInterfaceItGenerator() {
 		return null;
+	}
+
+	/**
+	 * @return the sourceArchivePath
+	 */
+	public String getSourceArchivePath() {
+		return sourceArchivePath;
+	}
+
+	/**
+	 * @param sourceArchivePath
+	 *            the sourceArchivePath to set
+	 */
+	public void setSourceArchivePath(String sourceArchivePath) {
+		this.sourceArchivePath = sourceArchivePath;
+	}
+
+	/**
+	 * @param delegateClass
+	 *            the delegateClass to set
+	 */
+	public void setDelegateClass(String delegateClass) {
+		this.delegateClass = delegateClass;
+	}
+
+	/**
+	 * @return the delegateClass
+	 */
+	public String getDelegateClass() {
+		return delegateClass;
 	}
 
 }
--- src/test/java/com/github/aro_tech/interface_it_ant/InterfaceItTaskTest.java
+++ src/test/java/com/github/aro_tech/interface_it_ant/InterfaceItTaskTest.java
@@ -1,34 +1,58 @@
 package com.github.aro_tech.interface_it_ant;
 
-import static org.junit.Assert.fail;
+import java.io.IOException;
+import java.lang.reflect.Method;
 
+import org.interfaceit.meta.arguments.ArgumentNameSource;
 import org.junit.Before;
 import org.junit.Test;
 
+import com.github.aro_tech.interface_it_ant.wrappers.AssertJ;
+import com.github.aro_tech.interface_it_ant.wrappers.Mockito;
+
 /**
- * Unit tests for InterfaceItTask
+ * Unit and integration tests for InterfaceItTask
+ * 
  * @author aro_tech
  *
  */
-public class InterfaceItTaskTest {
+public class InterfaceItTaskTest implements AssertJ, Mockito {
+	InterfaceItTask underTest = new InterfaceItTask();
 
 	@Before
 	public void setUp() throws Exception {
 	}
 
+	// @Test
+	// public void testExecute() {
+	// fail("Not yet implemented");
+	// }
+
 	@Test
-	public void testExecute() {
-		fail("Not yet implemented");
+	public void should_construct_default_argument_name_source_if_no_source_provided()
+			throws NoSuchMethodException, SecurityException, ClassNotFoundException, IOException {
+		underTest.setDelegateClass("java.lang.Math");
+		ArgumentNameSource result = underTest.makeArgumentNameSource();
+		Method method = org.mockito.Mockito.class.getMethod("mock", Class.class);
+		assertThat(result.getArgumentNameFor(method, 0)).isEqualTo("arg0");
 	}
 
 	@Test
-	public void testMakeArgumentNameSource() {
-		fail("Not yet implemented");
+	public void should_construct_argument_name_source_from_zip_INTEGRATION()
+			throws NoSuchMethodException, SecurityException, ClassNotFoundException, IOException {
+		underTest.setDelegateClass(org.mockito.Mockito.class.getName());
+
+		String path = this.getClass().getClassLoader().getResource("examples.zip").toExternalForm().substring(6);
+		underTest.setSourceArchivePath(path);
+
+		ArgumentNameSource result = underTest.makeArgumentNameSource();
+		Method method = org.mockito.Mockito.class.getMethod("mock", Class.class);
+		assertThat(result.getArgumentNameFor(method, 0)).isEqualTo("classToMock");
 	}
 
-	@Test
-	public void testMakeInterfaceItGenerator() {
-		fail("Not yet implemented");
-	}
+	// @Test
+	// public void testMakeInterfaceItGenerator() {
+	// fail("Not yet implemented");
+	// }
 
 }
